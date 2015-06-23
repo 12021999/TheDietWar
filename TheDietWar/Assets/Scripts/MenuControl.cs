3b90a06402e300aa447668d6c3e7688f22ea6670
12021999 <matolisou@gmail.com>
2015-06-23 08:58:57 -0300

    The Diet War
    Menu
    butoes
    jogo
    movimentacao

---
+++ TheDietWar/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+
+    public GameObject[] Buttons;
+	void Start () 
+    {
+        
+	}
+
+    void ChangeScenes()
+    {
+        if(!Buttons[0].activeSelf)
+        {
+            Application.LoadLevel("Game");
+        }
+    }
+
+	void Update () 
+    {
+        print(Buttons[0]);
+        ChangeScenes();
+	}
+}
---
+++ TheDietWar/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+    void Movimentation()
+    {
+        if (Input.acceleration.x > 0)
+        {
+            rigidbody2D.velocity = new Vector2(Input.acceleration.x * 0.5f,0);
+        }
+        else if (Input.acceleration.x < 0)
+        {
+            rigidbody2D.velocity = new Vector2(Input.acceleration.x * -0.5f, 0);
+        }
+    }
+
+	void FixedUpdate () 
+    {
+        Movimentation();
+	}
+}
